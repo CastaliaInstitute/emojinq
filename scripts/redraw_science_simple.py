e8efeb266ee728e9270feafad25774f1bdde5044
dcmcshan <dan@syzygyx.com>
2026-08-09 05:25:20 -0400

    Expand simplified science glyphs

--- scripts/redraw_science_simple.py
+++ scripts/redraw_science_simple.py
@@ -64,6 +64,51 @@ ART = {
 <path d="M36 12v48M16 24l40 24M16 48l40-24M23 16l13 20 13-20M23 56l13-20 13 20" stroke-width="1.75" {i}/>
 <path d="M36 12l-3 5M36 12l3 5M16 24l6 1M16 24l3 5M16 48l6-1M16 48l3-5M56 24l-6 1M56 24l-3 5M56 48l-6-1M56 48l-3-5" stroke-width="1.05" {i}/>
 """,
+    "mentor": """
+<circle cx="25" cy="24" r="5" stroke-width="1.75" {i}/><circle cx="47" cy="30" r="5" stroke-width="1.75" {i}/>
+<path d="M16 53c1-10 6-16 13-16s11 6 12 16M39 55c1-8 5-13 11-13 5 0 8 4 9 11" stroke-width="2.0" {i}/>
+<path d="M31 25c5-4 9-4 14 0M34 20l5-5 5 5" stroke-width="1.35" {i}/>
+""",
+    "legend": """
+<path d="M14 20c10-5 21-5 32 0l12-5v34l-12 5c-11-5-22-5-32 0z" stroke-width="2.1" {i}/>
+<path d="M46 20v34M26 26c5-2 10-2 15 0M25 34c5-2 10-2 15 0" stroke-width="1.25" {i}/>
+<path d="M20 47l4-5 4 5 5-2" stroke-width="1.1" {i}/>
+""",
+    "flow": """
+<path d="M9 28c8-9 17-9 24 0 7 9 15 9 29-1M9 43c8-9 17-9 24 0 7 9 15 9 29-1M17 57c7-5 14-5 21 0" stroke-width="2.15" {i}/>
+<path d="M48 18c4-3 8-3 13 0" stroke-width="1.15" {i}/>
+""",
+    "contradiction": """
+<path d="M21 54c-5-8-3-16 3-22 4-4 5-10 3-16 8 6 10 13 6 20-3 6-2 12 3 18" stroke-width="2.15" {i}/>
+<path d="M49 15v38M34 23l30 18M34 41l30-18M40 18l9 18 9-18M40 48l9-18 9 18" stroke-width="1.35" {i}/>
+""",
+    "cause": """
+<path d="M36 13c-5 5-7 9-7 13 0 5 3 8 7 8s7-3 7-8c0-4-2-8-7-13z" stroke-width="1.8" {i}/>
+<path d="M36 36c-10 4-16 9-16 15M36 36c10 4 16 9 16 15M12 56c8-3 16-3 24 0 8-3 16-3 24 0" stroke-width="1.55" {i}/>
+""",
+    "divide": """
+<path d="M36 12v48" stroke-width="2.0" {i}/>
+<path d="M10 23c8 4 16 4 24 0M10 49c8-4 16-4 24 0M62 23c-8 4-16 4-24 0M62 49c-8-4-16-4-24 0" stroke-width="1.4" {i}/>
+<path d="M13 59c7-2 14-2 21 0M59 59c-7-2-14-2-21 0" stroke-width="1.0" opacity=".65" {i}/>
+""",
+    "mist": """
+<path d="M10 33c7-7 14-7 21 0 6-7 14-7 21 0 5-4 10-4 14 0M10 45c8-4 16-4 24 0 8-4 16-4 28 0M16 56c7-3 14-3 21 0 7-3 14-3 20 0" stroke-width="1.65" {i}/>
+""",
+    "history": """
+<circle cx="30" cy="35" r="18" stroke-width="2.1" {i}/>
+<path d="M30 22v14l8 6M14 21l-5 5M9 26h7" stroke-width="1.55" {i}/>
+<path d="M47 47c6 2 9 6 10 11M54 52l4 1-1 4" stroke-width="1.35" {i}/>
+""",
+    "knowledge": """
+<path d="M12 22c8-3 16-2 24 4 8-6 16-7 24-4v35c-8-3-16-2-24 4-8-6-16-7-24-4z" stroke-width="2.1" {i}/>
+<path d="M36 26v35M19 31c5-2 10-1 15 2M53 31c-5-2-10-1-15 2" stroke-width="1.25" {i}/>
+<path d="M54 15l1 4M50 18l4 1M58 18l-3 1" stroke-width="1.15" {i}/>
+""",
+    "future": """
+<path d="M36 59V33M36 42c-7-8-13-9-19-6 2 8 8 12 19 11M36 38c7-8 13-9 19-6-2 8-8 12-19 11" stroke-width="2.0" {i}/>
+<path d="M10 59c9-3 18-3 26 0 8-3 17-3 26 0" stroke-width="1.15" {i}/>
+<path d="M36 19l1 4M31 22l5-3 5 3" stroke-width="1.1" {i}/>
+""",
 }
 
 
