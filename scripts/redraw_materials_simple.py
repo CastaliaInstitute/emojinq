b8a1d9338ea006b17ddd0748c8fb601d9676c216
dcmcshan <dan@syzygyx.com>
2026-08-09 05:08:16 -0400

    Refine material PUA distinctions

--- scripts/redraw_materials_simple.py
+++ scripts/redraw_materials_simple.py
@@ -12,14 +12,14 @@ INK = 'fill="none" stroke="#262522" stroke-linecap="round" stroke-linejoin="roun
 
 ART = {
     "clay": """
-<path d="M21 22h30l-3 33c-1 5-8 8-12 8s-11-3-12-8z" stroke-width="2.3" {i}/>
-<path d="M18 22c0-4 5-6 18-6s18 2 18 6-5 6-18 6-18-2-18-6z" stroke-width="2.0" {i}/>
-<path d="M24 39c7 3 17 3 25 0M24 49c7 3 17 3 25 0" stroke-width="1.15" {i}/>
+<path d="M25 22h22l-2 8c8 5 10 13 8 22-2 8-9 12-17 12s-15-4-17-12c-2-9 1-17 8-22z" stroke-width="2.3" {i}/>
+<path d="M23 22c0-4 4-6 13-6s13 2 13 6-4 5-13 5-13-1-13-5z" stroke-width="2.0" {i}/>
+<path d="M21 45c9 4 21 4 30 0M23 55c8 3 18 3 26 0" stroke-width="1.15" {i}/>
 """,
     "cloth": """
-<path d="M12 25l13-8 36 13-14 27-36-12z" stroke-width="2.2" {i}/>
-<path d="M25 17l22 13 14 0M25 17l-1 28M24 45l23 12" stroke-width="1.4" {i}/>
-<path d="M18 29c8 3 15 7 22 12M20 38c7 3 13 6 20 11" stroke-width="1.0" opacity=".75" {i}/>
+<path d="M13 23c8-4 16-4 23 0 8 4 15 4 23 0l1 28c-10 6-20 6-29 0-7-4-12-4-19 0z" stroke-width="2.2" {i}/>
+<path d="M13 23c8 5 16 5 23 0 8 5 15 5 23 0M14 38c7 4 14 4 22 0 8 4 15 4 24 0" stroke-width="1.3" {i}/>
+<path d="M19 29c2 2 4 3 7 3M43 31c3 1 5 1 8-1" stroke-width="1.0" opacity=".7" {i}/>
 """,
     "fiber": """
 <path d="M17 57c8-11 12-22 18-39M25 58c8-12 14-22 24-35M34 59c8-12 16-20 24-28" stroke-width="2.0" {i}/>
@@ -41,9 +41,9 @@ ART = {
 <path d="M17 25l8 27" stroke-width="1.0" opacity=".65" {i}/>
 """,
     "paper": """
-<path d="M15 20l39 5 4 32-39-5z" stroke-width="2.1" {i}/>
-<path d="M15 20l18 18 25-13M33 38l-14 14M33 38l25 19" stroke-width="1.35" {i}/>
-<path d="M23 28l10 2M22 34l9 2" stroke-width="1.0" {i}/>
+<path d="M15 18l43 6v35l-43-6z" stroke-width="2.1" {i}/>
+<path d="M15 18l20 19 23-13M35 37l-16 13M35 37l23 22" stroke-width="1.35" {i}/>
+<path d="M23 29l9 2M22 35l9 2M21 42l8 2" stroke-width="1.0" {i}/>
 """,
     "plastic": """
 <path d="M28 18h16v7l5 6v25c0 4-5 7-13 7s-13-3-13-7V31l5-6z" stroke-width="2.2" {i}/>
@@ -59,9 +59,9 @@ ART = {
 <path d="M23 38c7-3 15-3 24 0M21 48c9-2 17-2 27 1" stroke-width="1.15" {i}/>
 """,
     "thread": """
-<path d="M21 21h30v33c-2 6-8 9-15 9s-13-3-15-9z" stroke-width="2.2" {i}/>
-<path d="M21 21c0-4 7-6 15-6s15 2 15 6-7 6-15 6-15-2-15-6zM28 35c6 4 12 4 18 0M28 44c6 4 12 4 18 0" stroke-width="1.2" {i}/>
-<path d="M51 27c8 2 11 6 10 12-1 5-5 8-10 8" stroke-width="1.5" {i}/>
+<path d="M24 22c3-5 8-7 12-7s9 2 12 7v30c-3 5-7 8-12 8s-9-3-12-8z" stroke-width="2.2" {i}/>
+<path d="M24 22c0 5 5 8 12 8s12-3 12-8M24 52c0-5 5-8 12-8s12 3 12 8M29 30v14M43 30v14" stroke-width="1.2" {i}/>
+<path d="M48 27c8 2 11 6 10 12-1 5-5 8-10 8" stroke-width="1.5" {i}/>
 """,
 }
 
